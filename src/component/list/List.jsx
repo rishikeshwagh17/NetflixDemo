4e57a40716664aa1a427cfd10f5e73c33ab543bd
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-03-29 18:58:48 +0530

    movie hovering features and trailer added

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -26,16 +26,16 @@ export default function List() {
         <div className="wrapper">
             <ArrowBackIosOutlinedIcon className='sliderArrow left' onClick={()=>handleClick("left")} style={{display:!isMoved && "none"}}/>
             <div className="container" ref={listRef}>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
-                <Listitem/>
+                <Listitem index={0}/>
+                <Listitem index={1}/>
+                <Listitem index={2}/>
+                <Listitem index={3}/>
+                <Listitem index={4}/>
+                <Listitem index={5}/>
+                <Listitem index={6}/>
+                <Listitem index={7}/>
+                <Listitem index={8}/>
+                <Listitem index={9}/>
             </div>
             <ArrowForwardIosOutlinedIcon className="sliderArrow right" onClick={()=>handleClick("right")}/>
         </div>
